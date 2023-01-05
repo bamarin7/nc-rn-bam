b62dff98ef189d70185135ec8a949f3236f30d49
Brian Marin Silva <marinbrian.as@gmail.com>
2023-01-04 18:47:25 -0600

    Adding Contact and About to the navigator

---
+++ screens/ContactScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+const ContactScreen = () => {
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    );
+};
+
+export default ContactScreen;
